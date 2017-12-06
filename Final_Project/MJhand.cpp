76364fc9e8856bc3aeea6cebba6b6f3f2d424318
Harry Chang <harrychang1017@gmail.com>
2017-12-06 19:49:39 +0800

    Add files via upload

---
+++ Final_Project/MJhand.cpp
@@ -0,0 +1,373 @@
+#include <iostream> 
+#include <algorithm>
+using namespace std;
+
+#include "MJhand.h"
+
+
+bool MJcompare(const MJtile& i, const MJtile& j) { 
+}
+
+MJhand::MJhand(){}
+MJhand::MJhand(MJtile* t, int n){
+}
+MJhand::~MJhand(){
+}
+
+int MJhand::faceup_len() const {
+	return 0;
+}
+int MJhand::caneat(const MJtile& t){
+	return 0;
+}
+bool MJhand::canpong(const MJtile& t){
+	return false;
+}
+bool MJhand::cangone(const MJtile& t){
+	return false;
+}
+
+void MJhand::arrange(){	
+}
+void MJhand::draw(MJtile* mjtiles, int& frontind, int& backind){
+}
+void MJhand::play(int index){
+}
+void MJhand::faceup(int index){
+}
+void MJhand::applique(int index, MJtile* mjtiles, int& frontind, int& backind){
+}
+void MJhand::initial(MJtile* mjtiles, int& frontind, int& backind){
+}
+void MJhand::eat(const MJtile& t){
+}
+void MJhand::pong(const MJtile& t){
+}
+void MJhand::gone(const MJtile& t){
+}
+/*
+ostream& operator << (ostream& os, const MJhand& h){
+	
+	//part 1 " __ "
+	for(int i=0;i<h._total_len+h._stage;i++){
+		if(i==h.faceup_len())
+			os<<"    ";//4 spaces
+		if(h._stage && i==h._total_len+h._stage-1)
+			os<<"    ";//4 spaces
+		os<<" __ ";
+	}
+	os<<endl;
+	
+	//part 2 "| N|"
+	for(int i=0;i<h._total_len+h._stage;i++){
+		if(i==h.faceup_len())
+			os<<"    ";//4 spaces
+		if(h._stage && i==h._total_len+h._stage-1)
+			os<<"    ";//4 spaces
+		os<<"|";
+		if(h._tiles[i].suit()==4){ 
+			if(h._tiles[i].flower())
+				os<<" "<<h._tiles[i].flower();
+			else 
+				os<<"  ";
+		} 
+		else{
+			switch(h._tiles[i].rank()){
+				case 1:
+					os<<" 1";
+					break; 
+				case 2:
+					os<<" 2";
+					break; 
+				case 3:
+					os<<" 3";
+					break; 
+				case 4:
+					os<<" 4";
+					break; 
+				case 5:
+					os<<" 5";
+					break; 
+				case 6:
+					os<<" 6";
+					break; 
+				case 7:
+					os<<" 7";
+					break; 
+				case 8:
+					os<<" 8";
+					break; 
+				case 9:
+					os<<" 9";
+					break; 
+				default:
+					assert(false); 
+			}
+		} 
+		os<<"|";
+	}
+	os<<endl;
+	
+	//part 3 "|XX|"
+	for(int i=0;i<h._total_len+h._stage;i++){
+		if(i==h.faceup_len())
+			os<<"    ";//4 spaces
+		if(h._stage && i==h._total_len+h._stage-1)
+			os<<"    ";//4 spaces
+		os<<"|";
+		if(h._tiles[i].suit()==4)
+			switch(h._tiles[i].rank()){
+				case 1:
+					os<<"Ea";
+					break; 
+				case 2:
+					os<<"So";
+					break; 
+				case 3:
+					os<<"We";
+					break; 
+				case 4:
+					os<<"No";
+					break; 
+				case 5:
+					os<<"Mi";
+					break; 
+				case 6:
+					os<<"Fa";
+					break; 
+				case 7:
+					os<<"  ";
+					break; 
+				case 8:
+					switch(h._tiles[i].flower()){
+						case 1:
+							os<<"Me";
+							break; 
+						case 2:
+							os<<"La";
+							break; 
+						case 3:
+							os<<"Zu";
+							break; 
+						case 4:
+							os<<"Ju";
+							break; 
+						default:
+							assert(false); 
+					} 
+					break; 
+				case 9:
+					switch(h._tiles[i].flower()){
+						case 1:
+							os<<"Sp";
+							break; 
+						case 2:
+							os<<"Su";
+							break; 
+						case 3:
+							os<<"Au";
+							break; 
+						case 4:
+							os<<"Wi";
+							break; 
+						default:
+							assert(false); 
+					} 
+					break; 
+				default:
+					assert(false); 
+			}
+		else{
+			switch(h._tiles[i].suit()){
+				case 1:
+					os<<" O";
+					break; 
+				case 2:
+					os<<" I";
+					break; 
+				case 3:
+					os<<" W";
+					break; 
+				default:
+					assert(false); 
+			}
+		} 
+		os<<"|";
+	}
+	os<<endl;
+	
+	//part 4 " -- "
+	for(int i=0;i<h._total_len+h._stage;i++){
+		if(i==h.faceup_len())
+			os<<"    ";//4 spaces
+		if(h._stage && i==h._total_len+h._stage-1)
+			os<<"    ";//4 spaces
+		os<<" -- ";
+	}
+	os<<endl; 
+	return os;
+} 
+//*/
+///* 
+ostream& operator << (ostream& os, const MJhand& h){
+	
+	//part 1 " __ "
+	for(int i=0;i<h._total_len+h._stage;i++){
+		if(i==h.faceup_len())
+			os<<"    ";//4 spaces
+		if(h._stage && i==h._total_len+h._stage-1)
+			os<<"    ";//4 spaces
+		os<<" __ ";
+	}
+	os<<endl;
+	
+	//part 2 "| N|"
+	for(int i=0;i<h._total_len+h._stage;i++){
+		if(i==h.faceup_len())
+			os<<"    ";//4 spaces
+		if(h._stage && i==h._total_len+h._stage-1)
+			os<<"    ";//4 spaces
+		os<<"|";
+		if(h._tiles[i].suit()==4){ 
+			if(h._tiles[i].flower())
+				os<<" "<<h._tiles[i].flower();
+			else 
+				os<<"  ";
+		} 
+		else{
+			switch(h._tiles[i].rank()){
+				case 1:
+					os<<"一";
+					break; 
+				case 2:
+					os<<"二";
+					break; 
+				case 3:
+					os<<"三";
+					break; 
+				case 4:
+					os<<"四";
+					break; 
+				case 5:
+					os<<"五";
+					break; 
+				case 6:
+					os<<"六";
+					break; 
+				case 7:
+					os<<"七";
+					break; 
+				case 8:
+					os<<"八";
+					break; 
+				case 9:
+					os<<"九";
+					break; 
+				default:
+					assert(false); 
+			}
+		} 
+		os<<"|";
+	}
+	os<<endl;
+	
+	//part 3 "|XX|"
+	for(int i=0;i<h._total_len+h._stage;i++){
+		if(i==h.faceup_len())
+			os<<"    ";//4 spaces
+		if(h._stage && i==h._total_len+h._stage-1)
+			os<<"    ";//4 spaces
+		os<<"|";
+		if(h._tiles[i].suit()==4)
+			switch(h._tiles[i].rank()){
+				case 1:
+					os<<"東";
+					break; 
+				case 2:
+					os<<"南";
+					break; 
+				case 3:
+					os<<"西";
+					break; 
+				case 4:
+					os<<"北";
+					break; 
+				case 5:
+					os<<"中";
+					break; 
+				case 6:
+					os<<"發";
+					break; 
+				case 7:
+					os<<"  ";
+					break; 
+				case 8:
+					switch(h._tiles[i].flower()){
+						case 1:
+							os<<"梅";
+							break; 
+						case 2:
+							os<<"蘭";
+							break; 
+						case 3:
+							os<<"竹";
+							break; 
+						case 4:
+							os<<"菊";
+							break; 
+						default:
+							assert(false); 
+					} 
+					break; 
+				case 9:
+					switch(h._tiles[i].flower()){
+						case 1:
+							os<<"春";
+							break; 
+						case 2:
+							os<<"夏";
+							break; 
+						case 3:
+							os<<"秋";
+							break; 
+						case 4:
+							os<<"冬";
+							break; 
+						default:
+							assert(false); 
+					} 
+					break; 
+				default:
+					assert(false); 
+			}
+		else{
+			switch(h._tiles[i].suit()){
+				case 1:
+					os<<"筒";
+					break; 
+				case 2:
+					os<<"條";
+					break; 
+				case 3:
+					os<<"萬";
+					break; 
+				default:
+					assert(false); 
+			}
+		} 
+		os<<"|";
+	}
+	os<<endl;
+	
+	//part 4 " -- "
+	for(int i=0;i<h._total_len+h._stage;i++){
+		if(i==h.faceup_len())
+			os<<"    ";//4 spaces
+		if(h._stage && i==h._total_len+h._stage-1)
+			os<<"    ";//4 spaces
+		os<<" ￣ ";
+	}
+	os<<endl; 
+	return os;
+} 
+//*/
