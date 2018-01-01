18ae30c58cb34f89be9d5ce96b2aa22bcf0e9d13
Harry Chang <harrychang1017@gmail.com>
2018-01-01 12:32:21 +0800

    Update Shuffler.h

--- Final_Project/Shuffler.h
+++ Final_Project/Shuffler.h
@@ -10,36 +10,30 @@ using namespace std;
 
 class Shuffler{
 public:
-	Shuffler();
-	void init();
-	void fill(MJtile tiles[])const;
+	Shuffler(){
+		MJtile tmp;
+		for(int i=0;i<144;++i){
+			_tiles[i]=tmp;
+		}
+	}
+	void init(){
+		vector<int> v;
+		for(int i=1;i<=144;++i){
+			v.push_back(i);
+		}
+		random_shuffle(v.begin(),v.end());
+		for(int i=0;i<144;++i){
+			MJtile tmp(v[i]);
+			_tiles[i]=tmp;
+		}
+	}
+	void fill(MJtile tiles[])const{
+		for(int i=0;i<144;++i){
+			tiles[i]=_tiles[i];
+		}
+	}
 private:
 	MJtile _tiles[144];
 };
-
-Shuffler::Shuffler(){
-	MJtile tmp;
-	for(int i=0;i<144;++i){
-		_tiles[i]=tmp;
-	}
-}
-
-void Shuffler::init(){
-	vector<int> v;
-	for(int i=1;i<=144;++i){
-		v.push_back(i);
-	}
-	random_shuffle(v.begin(),v.end());
-	for(int i=0;i<144;++i){
-		MJtile tmp(v[i]);
-		_tiles[i]=tmp;
-	}
-}
-
-void Shuffler::fill(MJtile tiles[])const{
-	for(int i=0;i<144;++i){
-		tiles[i]=_tiles[i];
-	}
-}
 
 #endif
